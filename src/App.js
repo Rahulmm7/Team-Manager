3890479e4c22d09506e0909a3e7760e7063729a7
Rahul M M <rahulmanohar007@gmail.com>
2023-01-04 18:02:29 +0530

    member card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./Components/Header";
 import Content from "./Components/Content";
 import Footer from "./Components/Footer";
+import Employess from "./Components/Employees";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Employess />
       <Content />
       <Footer />
 
--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -1,9 +1,121 @@
 import { useState } from "react"
-
+import femaleProfile from '../images/femaleProfile.jpg'
+import './style/Employee.scss'
 const Employess = () => {
-    const [employee, useEmployee] = useState({ "id": 1, "FullName": "Lionel Messi" })
+
+    const [employees, setEmployees] = useState([{
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "TeamA"
+    },
+    {
+        id: 2,
+        fullName: "Jill Bailey",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "TeamA"
+    },
+    {
+        id: 3,
+        fullName: "Gail Shepherd",
+        designation: "Java Developer",
+        gender: "female",
+        teamName: "TeamA"
+    },
+    {
+        id: 4,
+        fullName: "Sam Reynolds",
+        designation: "React Developer",
+        gender: "male",
+        teamName: "TeamB"
+    },
+    {
+        id: 5,
+        fullName: "David Henry",
+        designation: "DotNet Developer",
+        gender: "male",
+        teamName: "TeamB"
+    },
+    {
+        id: 6,
+        fullName: "Sarah Blake",
+        designation: "SQL Server DBA",
+        gender: "female",
+        teamName: "TeamB"
+    },
+    {
+        id: 7,
+        fullName: "James Bennet",
+        designation: "Angular Developer",
+        gender: "male",
+        teamName: "TeamC"
+    },
+    {
+        id: 8,
+        fullName: "Jessica Faye",
+        designation: "API Developer",
+        gender: "female",
+        teamName: "TeamC"
+    },
+    {
+        id: 9,
+        fullName: "Lita Stone",
+        designation: "C++ Developer",
+        gender: "female",
+        teamName: "TeamC"
+    },
+    {
+        id: 10,
+        fullName: "Daniel Young",
+        designation: "Python Developer",
+        gender: "male",
+        teamName: "TeamD"
+    },
+    {
+        id: 11,
+        fullName: "Adrian Jacobs",
+        designation: "Vue Developer",
+        gender: "male",
+        teamName: "TeamD"
+    },
+    {
+        id: 12,
+        fullName: "Devin Monroe",
+        designation: "Graphic Designer",
+        gender: "male",
+        teamName: "TeamD"
+    }]);
 
     return (
-        <div></div>
+        <main className="container">
+            <div className="row justify-content-center mb-3 mt-3">
+                <div className="col-8">
+                    <div className="card-collection">
+
+                        {
+                            employees.map((employee) => (
+                                <div id={employee.id} className="card m-2">
+                                    <img src={femaleProfile} className="card-img-top" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            Full Name : {employee.fullName}
+                                        </h5>
+                                        <p className="card-description">
+                                            Designation : {employee.designation}
+                                        </p>
+
+                                    </div>
+                                </div>
+
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
+
+export default Employess
